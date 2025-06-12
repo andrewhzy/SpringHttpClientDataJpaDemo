181c7ef6ada0da9fe3155b92758090ebb5ac3533
he zhengyu <hezhengyu.hzy@gmail.com>
2025-06-12 14:35:30 +0800

    initial commit

---
+++ src/test/java/com/example/springhttpclientdatajpademo/SpringHttpClientDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhttpclientdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpClientDataJpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
